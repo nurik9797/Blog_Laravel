ea0ddd41ba1ba87e03d6591df5c80e5ed1bc20c9
Nurbek <nurbek.kozhobekov@yahoo.com>
2022-04-04 11:47:20 +0600

    Added my project

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,80 @@
+<footer class="bg-gray-800 py-20 my-20">
+    <div class="sm:grid grid-cols-3 w-25 pb-10 m-auto border-b-2 border-gray-700">
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Pages
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/Home">
+                    Home</a>    
+                </li>
+                <li class="pb-1">
+                    <a href="/Blog">
+                    Blog</a>    
+                </li>
+                <li class="pb-1">
+                    <a href="/Login">
+                    Login</a>    
+                </li>
+                <li class="pb-1">
+                    <a href="/Register">
+                    Register</a>    
+                </li>
+
+            </ul>
+        </div>
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Find us
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                    What we do</a>    
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                    Adress</a>    
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                    Phone</a>    
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                    Contact</a>    
+                </li>
+
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Latest posts
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                    wy we love it</a>    
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                    Wy u should use laravel</a>    
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                    Why PHP</a>    
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                    Why Design</a>    
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</footer>

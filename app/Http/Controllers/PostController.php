acb77468f8f1b4cefa2fd1496e3947caa5a83ae5
Nurbek <nurbek.kozhobekov@yahoo.com>
2022-04-05 10:12:06 +0600

    new

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Promise\Create;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -82,3 +83,6 @@ class PostController extends Controller
         //
     }
 }
+
+
+

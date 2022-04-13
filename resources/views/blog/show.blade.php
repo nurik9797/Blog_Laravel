949cb66331c4e84b1ddfcb81b2a94c045e57ca5d
Nurbek <nurbek.kozhobekov@yahoo.com>
2022-04-13 16:05:54 +0600

    new

---
+++ resources/views/blog/show.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="w-3/5 m-auto text-center italic font-serif">
+    <div class="py-15">
+        <h1 class="text-6xl">
+            {{$post->title}}
+        </h1>
+    </div>
+
+</div>
+
+
+
+<div class="w-4/5 m-auto pt-20">
+    <span class="text-gray-500">
+        By <span class="font-bold italic text-gray-800"> {{$post->user->name}}
+
+        </span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
+    </span>
+
+    <p class="text-xl text-gray-700 pt- pb-10 leading-8 font-light">
+        {{$post->description}}
+    </p>
+</div>
+
+
+    
+
+
+
+
+@endsection
